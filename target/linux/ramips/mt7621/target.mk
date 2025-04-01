9b9b5fbe483a48a078a8078d4f247a169b9137f4
nickschecka <nickschecka@yahoo.de>
2025-04-01 23:48:44 +0200

    user
    Signed-off-by: nickschecka <nickschecka@yahoo.de>

--- target/linux/ramips/mt7621/target.mk
+++ target/linux/ramips/mt7621/target.mk
@@ -4,13 +4,12 @@
 
 SUBTARGET:=mt7621
 BOARDNAME:=MT7621 based boards
-FEATURES+=nand ramdisk rtc usb minor
+FEATURES+=nand ramdisk rtc usb
 CPU_TYPE:=24kc
-KERNELNAME:=vmlinux vmlinuz
-# make Kernel/CopyImage use $LINUX_DIR/vmlinuz
-IMAGES_DIR:=../../..
 
-DEFAULT_PACKAGES += wpad-basic-mbedtls uboot-envtools kmod-crypto-hw-eip93
+DEFAULT_PACKAGES += kmod-mt76
+
+KERNEL_PATCHVER:=4.4
 
 define Target/Description
 	Build firmware images for Ralink MT7621 based boards.
